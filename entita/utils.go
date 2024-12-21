42b4b94dea68bb2d2e87af97c0b422c8e10caa07
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-12-21 10:43:49 +0100

    update utils print

--- entita/utils.go
+++ entita/utils.go
@@ -2,7 +2,7 @@ package entita
 
 import "fmt"
 
-// Stampa il piano graficamente sulla console
+// Stampa il piano graficamente con assi numerati
 func (p *Piano) StampaGrafica() {
 	// Determina i limiti del piano
 	minX, maxX, minY, maxY := 0, 0, 0, 0
@@ -21,21 +21,32 @@ func (p *Piano) StampaGrafica() {
 		}
 	}
 
-	// Stampa il piano riga per riga
+	// Stampa il piano con l'asse Y numerato
 	for y := maxY; y >= minY; y-- {
+		fmt.Printf("%2d  ", y) // Etichetta dell'asse Y con meno spazi
 		for x := minX; x <= maxX; x++ {
 			key := [2]int{x, y}
 			if entita, ok := p.Mappa[key]; ok {
 				switch entita.(type) {
 				case *Automa:
-					fmt.Print("A ")
+					fmt.Print("A  ") // Rappresentazione dell'automa
 				case *Rettangolo:
-					fmt.Print("O ")
+					fmt.Print("O  ") // Rappresentazione dell'ostacolo
 				}
 			} else {
-				fmt.Print(". ")
+				fmt.Print(".  ") // Spazio vuoto
 			}
 		}
 		fmt.Println()
 	}
+
+	// Stampa l'asse X sotto il piano
+	fmt.Print("   ") // Spazi per allineare l'asse Y
+	for x := minX; x <= maxX; x++ {
+		fmt.Printf("%2d", x) // Meno spazio rispetto al contenuto
+		if x < maxX {
+			fmt.Print(" ") // Spazi tra i valori dell'asse X
+		}
+	}
+	fmt.Println()
 }

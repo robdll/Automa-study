b475d0a027e39dcffcd186b63210765e64f2fc55
Roberto Di Lillo <roberto.dilillo@garycom.com>
2025-01-03 20:04:52 +0100

    add test for overlapped obstacles

--- tests/ostacoli_test.go
+++ tests/ostacoli_test.go
@@ -55,3 +55,38 @@ func TestRimuoviOstacolo(t *testing.T) {
 		}
 	}
 }
+
+
+func TestOstacoliSovrapposti(t *testing.T) {
+	p := NuovoPiano()
+
+	// Aggiungi un primo ostacolo
+	err := p.AggiungiOstacolo(Rettangolo{
+			AngoloBassoSinistro: [2]int{1, 1},
+			AngoloAltoDestro:    [2]int{3, 3},
+	})
+	if err != nil {
+			t.Fatalf("Errore nell'aggiungere il primo ostacolo: %v", err)
+	}
+
+	// Aggiungi un secondo ostacolo che si sovrappone al primo
+	err = p.AggiungiOstacolo(Rettangolo{
+			AngoloBassoSinistro: [2]int{2, 2},
+			AngoloAltoDestro:    [2]int{4, 4},
+	})
+	if err != nil {
+			t.Fatalf("Errore nell'aggiungere il secondo ostacolo: %v", err)
+	}
+
+	// Stampa il piano per verifica manuale
+	p.Stampa()
+
+	// Verifica che le posizioni comuni siano ancora rappresentate come ostacoli
+	for x := 2; x <= 3; x++ {
+			for y := 2; y <= 3; y++ {
+					if _, ok := p.Mappa[[2]int{x, y}].(*Rettangolo); !ok {
+							t.Errorf("Posizione (%d, %d) non rappresenta un ostacolo sovrapposto", x, y)
+					}
+			}
+	}
+}

4a0a8f58130f729ae196fb97b06833d88a34a514
Roberto Di Lillo <roberto.dilillo@garycom.com>
2025-01-05 10:56:14 +0100

    update main

--- main.go
+++ main.go
@@ -8,9 +8,19 @@ func main() {
 
 	p := Crea()
 
-	p.AggiungiOstacolo(1, 1, 3, 3)
+	p.AggiungiOstacolo(3, 0, 15, 1)
+	p.AggiungiOstacolo(6, 0, 10, 3)
+	p.AggiungiOstacolo(3, 2, 4, 7)
+	p.AggiungiOstacolo(1, 3, 4, 6)
+	p.AggiungiOstacolo(6, 5, 7, 8)
 
-	p.PosizionaAutoma(0, 0, "0101")
+	p.PosizionaAutoma(2, 1, "1")
+	p.PosizionaAutoma(2, 8, "10")
+	p.PosizionaAutoma(5, 11, "11")
+	p.PosizionaAutoma(8, 9, "100")
+	p.PosizionaAutoma(10, 6, "101")
+	p.PosizionaAutoma(13, 3, "110")
+	p.PosizionaAutoma(12, 2, "111")
 
 	p.StampaGrafica()
 
